81429eadf857137ba4b5b7cd43f3ccd64f20b376
jake <dphattruong0510@gmail.com>
2026-04-15 22:50:56 +0700

    Add Marts Layer: 2 tables

---
+++ dbt_project/gold_dbt/models/marts/fct_gold_prices_daily.sql
@@ -0,0 +1,26 @@
+with staging as (
+
+    select *
+    from {{ ref('stg_gold_prices') }}
+
+),
+
+daily_metrics as (
+
+    select
+        brand,
+        price_date,
+        avg(buy_price) as avg_buy_price,
+        avg(sell_price) as avg_sell_price,
+        min(buy_price) as min_buy_price,
+        max(buy_price) as max_buy_price,
+        min(sell_price) as min_sell_price,
+        max(sell_price) as max_sell_price,
+        count(*) as data_points
+    from staging
+    group by 1, 2
+
+)
+
+select *
+from daily_metrics
---
+++ dbt_project/gold_dbt/models/marts/fct_gold_prices_hourly.sql
@@ -0,0 +1,26 @@
+with staging as (
+
+    select *
+    from {{ ref('stg_gold_prices') }}
+
+),
+
+hourly_metrics as (
+
+    select
+        brand,
+        date_trunc('hour', price_timestamp) as price_hour,
+        avg(buy_price) as avg_buy_price,
+        avg(sell_price) as avg_sell_price,
+        min(buy_price) as min_buy_price,
+        max(buy_price) as max_buy_price,
+        min(sell_price) as min_sell_price,
+        max(sell_price) as max_sell_price,
+        count(*) as data_points
+    from staging
+    group by 1, 2
+
+)
+
+select *
+from hourly_metrics
